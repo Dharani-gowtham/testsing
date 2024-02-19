1d4a2f878a276eaffc4d7b4680e80d55548e7af9
Dharani Gowtham S <dharaniadithya1998.da@gmail.com>
2024-02-19 06:59:13 +0530

    initial commit

--- main.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-num1 = st.number_input("Enter the Number 1")
-num2 = st.number_input("Enter the Number 2")
-if st.button("Add"):
-    st.write(f"# {num1+num2}")
-    st.balloons()
-st.file_uploader("Upload a CSV")
